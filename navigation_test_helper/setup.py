e80acfb324df4279940762d118386920041615fc
ct2034 <post@henkelchristian.de>
2014-05-27 14:18:09 +0200

    creating setup.py for naviagtion_test_helper and cleaning up

--- navigation_test_helper/setup.py
+++ navigation_test_helper/setup.py
@@ -5,11 +5,9 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
    ##  don't do this unless you want a globally visible script
-   scripts=['src/git.py'], 
+   # scripts=['bin/myscript'], 
    packages=['navigation_test_helper'],
    package_dir={'': 'src'}
 )
 
 setup(**d)
-
-## this is creates according to http://wiki.ros.org/catkin/migrating_from_rosbuild and http://docs.ros.org/api/catkin/html/user_guide/setup_dot_py.html
