94fc3a634c2d7bd47eff5368efeaad732e2ae124
ct2034 <post@henkelchristian.de>
2014-07-19 00:19:58 +0200

    implementing choosers

--- navigation_test_analysis/src/navigation_test_analysis/simple_viewer.py
+++ navigation_test_analysis/src/navigation_test_analysis/simple_viewer.py
@@ -1,20 +1,118 @@
 #!/usr/bin/env python
 
-import json, os, sys
+import json, os, sys, commands, re, string
 import pylab
 import matplotlib.pyplot as plt
 import numpy as np
 
 class SimpleViewer( object ):
 
-  def __init__( self, results_path ):
-    #self.repo = Repo(repo_path)
-    #assert self.repo.bare == False
+  def __init__( self, sshrepo, branch, navigation, robot, scene):
+    #self.sshrepo = sshrepo
+    #self.branch = branch
+    self.navigation = navigation
+    self.robot = robot
+    self.scene = scene
     
-    results_filen = results_path + "/2dnav_ipa_eband/cob3-6/scene2/result_16e4937f-6fcd-483b-9018-1c813924e246.bag.json"
-    print (">> results_path: " + results_path)
+    #self.config = [navigation, robot, scene]
     
-    results_file = open(results_path, "r").read()
+    self.path = self.clone(sshrepo, branch)
+  
+  
+  def clone(self, sshrepo, branch):
+    self.tmpdir = "/tmp"
+    self.reponame = re.search("(?<=/)[A-Za-z_]+(?=[.git])", sshrepo).group(0)
+  
+    #assert directory
+    ll_out = commands.getoutput("ls -a " + self.tmpdir + "/" + self.reponame);
+    
+    if ".git" in ll_out: # Repository exists
+      print "Pulling changes from " + self.reponame
+      print commands.getoutput("cd " + self.tmpdir + "/" + self.reponame + "; git pull origin " + branch)
+    elif "No such" in ll_out: # Folder doesn't exist
+      print "Cloning " + self.reponame
+      print commands.getoutput("cd " + self.tmpdir + "; git clone " + sshrepo)
+    else: # Folder exists but isn't a repo
+      print "Removing folder and cloning " + self.reponame
+      print commands.getoutput("rm -rf " + self.tmpdir + "/" + self.reponame)
+      print commands.getoutput("cd " + self.tmpdir + "; git clone " + sshrepo)
+      
+    return (self.tmpdir + "/" + self.reponame)
+    
+    
+  def listTests(self):
+    if "_" in [self.navigation, self.robot, self.scene]: # wildcard
+      print "Available tests: "
+      this_path = self.path
+      all_tests = []
+      
+      i = 1
+      if self.navigation is "_":
+        navs = string.split(commands.getoutput("ls " + this_path), "\n")
+        navs.pop(navs.index('README.md'))
+      else:
+        navs.append(self.navigation)
+      #print navs
+      
+      for nav in navs:
+      
+        if self.robot is "_":
+          robs = string.split(commands.getoutput("ls " + this_path + "/" + nav), "\n")
+        else:
+          robs = []
+          robs.append(self.robot)
+        #print robs
+        
+        for rob in robs:
+      
+          if self.navigation is "_":
+            scens = string.split(commands.getoutput("ls " + this_path + "/" + nav + "/" + rob), "\n")
+          else:
+            scens.append(self.scene)
+            
+          for scen in scens:
+          
+            print string.join([nav, rob, scen], " ")
+            
+            tests = string.split(commands.getoutput("ls " + this_path + "/" + nav + "/" + rob + "/" + scen), "\n")
+      
+            for test in tests:
+              print str(i) + ": " + test
+              i += 1
+              
+              all_test.append(test)
+        
+      inp = "N"
+      while inp not in range(1, i):
+        inp = input("please enter a number >")
+        
+                
+      return
+          
+    
+    
+    
+    else: # unambiguous parameter
+      print "Available tests for this combination: "
+      this_path = self.path + "/" + self.navigation + "/" + self.robot + "/"  + self.scene
+      tests = string.split(commands.getoutput("ls " + this_path), "\n")
+      
+      i = 1
+      for test in tests:
+        print str(i) + ": " + test
+        i += 1
+        
+      inp = "N"
+      while inp not in range(1, i):
+        inp = input("please enter a number >")
+        
+      return this_path + "/" + tests[inp-1]
+           
+           
+  def show(self, single_path):        
+    print (">> showing: " + single_path)
+    
+    results_file = open(single_path, "r").read()
     data_json = json.loads(results_file)[0]
     
     print ">> keys in the file:"
@@ -57,13 +155,20 @@ class SimpleViewer( object ):
     #plt.plot(deltas[:,0], deltas[:,1:4])
     #plt.plot(points[:,1], points[:,2])
     #plt.show()
-
     
+    return
   
 if __name__ == '__main__':
-  if len(sys.argv) < 2:
-    results_path = "$HOME/results/2dnav_ipa_eband/cob3-6/scene2/result_16e4937f-6fcd-483b-9018-1c813924e246.bag.json"
+  if len(sys.argv) is not 4:
+    print "Please pass the following arguments: navigation robot scene\nOr pass a '_' as a wildcard"
+ 
   else:
-    results_path = sys.argv[1]
-  
-  viewer = SimpleViewer(os.path.expandvars(results_path))
+    sshrepo= "git@github.com:ipa320/cob_navigation_tests_results.git"
+    branch = "master"
+    navigation = sys.argv[1]
+    robot = sys.argv[2]
+    scene = sys.argv[3]    
+         
+    viewer = SimpleViewer(sshrepo, branch, navigation, robot, scene)
+    single_path = viewer.listTests()
+    #viewer.show(single_path)
