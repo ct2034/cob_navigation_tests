afc2e91bb623f457460b9629062b25f9ea02f7cb
ct2034 <post@henkelchristian.de>
2014-07-24 13:01:09 +0200

    solving issue about starting scan unifier

--- nav_ground_truth_publisher/src/tf_repub.py
+++ nav_ground_truth_publisher/src/tf_repub.py
@@ -8,73 +8,44 @@ import tf
 
 class TfRepub:
         
-        
-    #   GROUND TRUTH TF --> NORMAL TF  
-    def callback_gttf(self, data):
-        tf_msg = TFMessage()
-        for tf in data.transforms:
-            for frame in self.frames_out:
-                if (frame == tf.header.frame_id) \
-                and self.is_new(tf.header.stamp, frame):
-                    tf.header.frame_id = self.prefix + tf.header.frame_id
-                    tf.header.stamp = rospy.Time.now()
-                    self.last_msg[frame] = rospy.Time.now()
-                    tf_msg.transforms.append(tf)
-                if (frame == tf.child_frame_id) \
-                and self.is_new(tf.header.stamp, frame):
-                    tf.child_frame_id = self.prefix + tf.child_frame_id
-                    tf.header.stamp = rospy.Time.now()
-                    self.last_msg[frame] = rospy.Time.now()
-                    tf_msg.transforms.append(tf)
-        self.tf_pub.publish(tf_msg)
-        #rospy.loginfo(tf_msg)
-        
-        
-    #   NORMAL TF --> GROUND TRUTH TF    
-    def callback_tf(self, data):
-        tf_msg = TFMessage()
-        for tf in data.transforms:
-            for frame in self.frames_in:
-                if ((frame == tf.header.frame_id) or (frame == tf.child_frame_id)) \
-                and self.is_new(tf.header.stamp, frame):
-                    tf.header.stamp = rospy.Time.now()
-                    self.last_msg[frame] = rospy.Time.now()
-                    tf_msg.transforms.append(tf)
-        self.gttf_pub.publish(tf_msg)
-        #rospy.loginfo("tf")
-        
-        
-    def is_new(self, time, frame):
-        return (time != self.last_msg[frame])
-        
-        
     def __init__(self):
         rospy.init_node('tf_repub', anonymous=True)
         
-        self.frame_odom = ["/odom_combined"]
-        self.frame_base = ["/base_footprint"]
-        self.frame_global = ["/map"]
-        self.frame_gt = ["/gazebo_gt"]
+        self.frame_odom = "/odom_combined"
+        self.frame_odom_gt = "/odom_gt"
+        self.frame_base = "/base_footprint"
+        self.frame_global = "/map"
+        self.frame_gt = "/gazebo_gt"
+        self.rate = 10.0
         
-        self.last_msg = dict.fromkeys( self.frames_in )
-        
-        self.prefix = "gt_"
-
         self.tf_list = tf.TransformListener()
-        rospy.Subscriber("tf", TFMessage, self.callback_tf)
-        rospy.loginfo("---> Set up listener to tf succesfully !!")
+        #rospy.Subscriber("tf", TFMessage, self.callback_tf)
+        rospy.loginfo("---> Set up tfListener succesfully !!")
+        self.tf_br = tf.TransformBroadcaster()
+        rospy.loginfo("---> Set up tfBroadcaster succesfully !!")
 
-        self.tf_pub = tf.TransformBroadcaster()
-        rospy.Subscriber("gt_tf", TFMessage, self.callback_gttf)
-        rospy.loginfo("---> Set up publisher to tf succesfully !!")
+        #self.tf_pub = tf.TransformBroadcaster()
+        #rospy.Subscriber("gt_tf", TFMessage, self.callback_gttf)
+        #rospy.loginfo("---> Set up publisher to tf succesfully !!")
 
+        rate = rospy.Rate(self.rate)
+        
         while not rospy.is_shutdown():
             try:
-                (trans,rot) = listener.lookupTransform('/turtle2', '/turtle1', rospy.Time(0))
-            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                (trans,rot) = self.tf_list.lookupTransform(self.frame_base, self.frame_odom, rospy.Time(0))
+                
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
+                #print "a tf Exception occured ..."
                 continue  
+                
+            #print "Callback trans[0] : " + str( trans[0] )
+            self.tf_br.sendTransform(trans, rot, rospy.Time.now(), self.frame_gt, self.frame_odom_gt)
+            
+            rate.sleep()
 
         rospy.spin()
+        
+      
     
         
 if __name__ == '__main__':
